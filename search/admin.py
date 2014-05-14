a40fbf213cb6e022fbe4231226e4c6af8bfa98d0
vindarel <ehvince@mailz.org>
2014-05-14 18:35:36 +0200

    admin ++

--- search/admin.py
+++ search/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from search.models import Card
+
 from search.models import Author
+from search.models import Card
+from search.models import Publisher
 
 admin.site.register(Card)
 admin.site.register(Author)
+admin.site.register(Publisher)
