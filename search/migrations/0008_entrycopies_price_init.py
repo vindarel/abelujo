d0be0af3daa505fb96372354bd7c5af25281b258
vindarel <ehvince@mailz.org>
2015-10-07 18:40:14 +0200

    migration: entry copies

---
+++ search/migrations/0008_entrycopies_price_init.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('search', '0007_entry_payment'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='entrycopies',
+            name='price_init',
+            field=models.FloatField(null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
