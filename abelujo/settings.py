20e3250981ce17e7642bf662fe14dd0d35afa4b0
vindarel <ehvince@mailz.org>
2015-11-10 23:09:17 +0100

    local bootstrap, without CDN -the end

--- abelujo/settings.py
+++ abelujo/settings.py
@@ -253,12 +253,12 @@ LOGGING = {
 LOGIN_URL = "/login/"
 
 # Don't get bootstrap and jquery from a CDN (a step towards offline work).
-# BOOTSTRAP3 = {
-#    'include_jquery': False,
-#    'jquery_url': '/static/bower_components/jquery/jquery.min.js',
-#    'base_url': '/static/',
-#    'css_url': '/static/lib/bootstrap/css/bootstrap.min.css',
-#    'theme_url': '/static/lib/bootstrap/css/bootstrap-theme.min.css',
-#    'javascript_url': '/static/lib/bootstrap/js/bootstrap.min.js',
-# }
-# BOOTSTRAP_BASE_URL = '/static/'
+BOOTSTRAP3 = {
+   'include_jquery': False,
+   'jquery_url': '/static/bower_components/jquery/jquery.min.js',
+   'base_url': '/static/',
+   'css_url': '/static/bower_components/bootstrap/dist/css/bootstrap.min.css',
+   'theme_url': '/static/bower_components/bootstrap/dist/css/bootstrap-theme.min.css',
+   'javascript_url': '/static/bower_components/bootstrap/dist/js/bootstrap.min.js',
+}
+BOOTSTRAP_BASE_URL = '/static/'
