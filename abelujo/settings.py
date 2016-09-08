d60f1ca46176cca9b373e461144360f42eed666e
vindarel <ehvince@mailz.org>
2016-09-08 21:54:29 +0200

    Install debug-toolbar and debug-panel (for ajax)

--- abelujo/settings.py
+++ abelujo/settings.py
@@ -187,10 +187,38 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     # Custom:
     'mod_wsgi.server',
+    'debug_toolbar',
+    'debug_panel',
 
     'search',
 )
 
+# DEBUG_TOOLBAR_PANELS = [
+#     'ddt_request_history.panels.request_history.RequestHistoryPanel',
+#     'debug_toolbar.panels.versions.VersionsPanel',
+#     'debug_toolbar.panels.timer.TimerPanel',
+#     'debug_toolbar.panels.settings.SettingsPanel',
+#     'debug_toolbar.panels.headers.HeadersPanel',
+#     'debug_toolbar.panels.request.RequestPanel',
+#     'debug_toolbar.panels.sql.SQLPanel',
+#     'debug_toolbar.panels.templates.TemplatesPanel',
+#     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+#     'debug_toolbar.panels.cache.CachePanel',
+#     'debug_toolbar.panels.signals.SignalsPanel',
+#     'debug_toolbar.panels.logging.LoggingPanel',
+#     'debug_toolbar.panels.redirects.RedirectsPanel',
+#     'debug_toolbar.panels.profiling.ProfilingPanel',
+# ]
+
+# DEBUG_TOOLBAR_CONFIG = {
+    # 'SHOW_TOOLBAR_CALLBACK': 'ddt_request_history.panels.request_history.allow_ajax',
+# }
+
+MIDDLEWARE_CLASSES += (
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_panel.middleware.DebugPanelMiddleware',
+    # 'abelujo.settings.NonHtmlDebugToolbarMiddleware',
+)
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 
