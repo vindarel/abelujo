8343d89e313ec9d335d59c9870d71611bfb97304
vindarel <ehvince@mailz.org>
2017-05-05 20:25:15 +0200

    Installation script, the easiest possible for end users.
    Aimed usage: right clic, run in terminal, watch the installation, see
    browser opening and software working.

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Install Abelujo, the easiest way as possible.
+# Usage: right clic, run this script in a terminal.
+# Supposed to be in a directory that does not already contain an Abelujo/ folder.
+
+LOCALHOST=http://localhost:8000
+VENV=abelujo
+
+
+venv_activate () {
+    source bin/activate
+}
+
+run_and_open () {
+    (make run &
+    make gulp
+    sleep 3 && xdg-open $LOCALHOST)
+}
+
+if [[ -d abelujo ]] ; then
+    cd abelujo
+    venv_activate
+    run_and_open
+
+else
+    echo "********* Do not use this script if you cloned this repository, it will not work. *********"
+    git clone --recursive http://gitlab.com/vindarel/abelujo/ || (echo "The installation failed. Feel free to email us the results." ; exit 1)
+
+    cd abelujo
+    echo "*******************************************************************************"
+    echo "*********  Please type your administrator password               **************"
+    echo "*********  (it is normal not to see the characters you type !)   **************"
+    echo "-------------------------------------------------------------------------------"
+    echo "--------- Veuillez entrer votre mot de passe super-utilisateur   --------------"
+    echo "--------- (c'est normal de ne pas voir ce que l'on tape !)       --------------"
+    echo "-------------------------------------------------------------------------------"
+    # let's go. We test the installation on CI, don't we ?!
+    make debian
+    make install
+    sudo make install-nodejs
+    virtualenv ./
+    venv_activate
+    pip install --upgrade pip
+    make install
+    run_and_open
+
+fi
+
+read  # let the terminal waiting, so open, in case of error.
