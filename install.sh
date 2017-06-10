e27e967a7b5d6d33a03b867a4b273a23d12bdd6d
vindarel <ehvince@mailz.org>
2017-06-10 16:13:21 +0200

    install.sh: install git

--- install.sh
+++ install.sh
@@ -18,6 +18,20 @@ run_and_open () {
     sleep 3 && xdg-open $LOCALHOST)
 }
 
+help_sudo_password () {
+    echo "*******************************************************************************"
+    echo "*********  Please type your administrator password               **************"
+    echo "*********  (it is normal not to see the characters you type !)   **************"
+    echo "-------------------------------------------------------------------------------"
+    echo "--------- Veuillez entrer votre mot de passe super-utilisateur   --------------"
+    echo "--------- (c'est normal de ne pas voir ce que l'on tape !)       --------------"
+    echo "-------------------------------------------------------------------------------"
+}
+
+install_git () {
+    type git || help_sudo_password && sudo apt-get install git-core
+}
+
 if [[ -d abelujo ]] ; then
     cd abelujo
     venv_activate
@@ -26,16 +40,11 @@ if [[ -d abelujo ]] ; then
 else
     echo "********* Installing Abelujo...                                       *********"
     echo "********* (if you cloned this repository, no need to use this script.)*********"
+    install_git
     git clone --recursive http://gitlab.com/vindarel/abelujo/ || (echo "The installation failed. Feel free to email us the results." ; exit 1)
 
     cd abelujo
-    echo "*******************************************************************************"
-    echo "*********  Please type your administrator password               **************"
-    echo "*********  (it is normal not to see the characters you type !)   **************"
-    echo "-------------------------------------------------------------------------------"
-    echo "--------- Veuillez entrer votre mot de passe super-utilisateur   --------------"
-    echo "--------- (c'est normal de ne pas voir ce que l'on tape !)       --------------"
-    echo "-------------------------------------------------------------------------------"
+    help_sudo_password
     # let's go. We test the installation on CI, don't we ?!
     make debian
     make install
