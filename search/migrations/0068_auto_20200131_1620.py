ea977f326b58bdbac8e5a342b4b88c84942dcc23
vindarel <vindarel@mailz.org>
2020-01-31 17:21:42 +0100

    migrations: payment methods, EntryCopies.price_init defaults to 0

---
+++ search/migrations/0068_auto_20200131_1620.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('search', '0067_auto_20200128_2236'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='entry',
+            name='payment',
+            field=models.CharField(blank=True, max_length=200, null=True, choices=[(1, 'cash'), (2, 'check'), (3, 'credit card'), (4, 'gift'), (5, 'transfer'), (6, 'other')]),
+        ),
+        migrations.AlterField(
+            model_name='entrycopies',
+            name='price_init',
+            field=models.FloatField(default=0.0, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='sell',
+            name='payment',
+            field=models.CharField(default=(1, 'cash'), choices=[(1, 'cash'), (2, 'check'), (3, 'credit card'), (4, 'gift'), (5, 'transfer'), (6, 'other')], max_length=200, blank=True, null=True, verbose_name='payment'),
+        ),
+    ]
