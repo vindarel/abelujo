4bc607398c0b742b75420c183ca2312fe67ea70c
vindarel <vindarel@mailz.org>
2020-02-05 18:29:12 +0100

    History sell cancel: confirm

--- search/static/search/js/abelujo-js.js
+++ search/static/search/js/abelujo-js.js
@@ -1,30 +1,28 @@
 
-function foo() {
-    alert("foo!!!");
-}
-
 function sellUndo(id) {
     // Undo the sell of the given id.
-    console.log("--- sellUndo id", id);
-    fetch(`/api/sell/${id}/undo`, {
-        method: 'POST',
-    })
-        .then((response) => {
-            console.log(response);
-            // console.log(response.text());
-            return response.json();
+    if (confirm("Voulez-vous annuler toute cette vente ?")){
+        console.log("--- sellUndo id", id);
+        fetch(`/api/sell/${id}/undo`, {
+            method: 'POST',
         })
-        .then((myJson) => {
-            console.log("json: ", myJson);
-            if (myJson.status == true) {
-                console.log('removing line.');
-                var lines = document.querySelectorAll(`#sell${id}`);
-                for (var i = 0; i < lines.length; i++) {
-                    lines[i].remove();
+            .then((response) => {
+                console.log(response);
+                // console.log(response.text());
+                return response.json();
+            })
+            .then((myJson) => {
+                console.log("json: ", myJson);
+                if (myJson.status == true) {
+                    console.log('removing line.');
+                    var lines = document.querySelectorAll(`#sell${id}`);
+                    for (var i = 0; i < lines.length; i++) {
+                        lines[i].remove();
+                    }
                 }
-            }
-        })
-        .catch((error) => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+            })
+            .catch((error) => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    }
 }
