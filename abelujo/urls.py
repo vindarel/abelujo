f151114d05579ef0c24d6b28a8e29b24cdc68b70
vindarel <vindarel@mailz.org>
2020-02-07 16:15:38 +0100

    update urls: use view callables, not strings

--- abelujo/urls.py
+++ abelujo/urls.py
@@ -15,120 +15,123 @@
 # along with Abelujo.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf import settings
 from django.conf.urls import include
-from django.conf.urls import patterns
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.contrib import auth
 
+from search.models import api
+# from search.urls import urlpatterns
 
 js_info_dict = {
     'packages': ('search', '',),
 }
 
-urlpatterns = i18n_patterns("", url("^", include("search.urls")))
+urlpatterns = i18n_patterns(url("^", include("search.urls", namespace=None)))
+# urlpatterns = []
 
-apipatterns = patterns("",
+apipatterns = [
     # pylint: disable=bad-continuation
     # Exclude /api/ from i18n so than we
     # don't bother of the language prefix in
     # js code.
     # XXX: the strings returned by the api (called from JS) are not translated. See issue #14.
-    url(r'^api/datasource/search/', 'search.models.api.datasource_search', name="api_search_datasource"),
+    url(r'^api/datasource/search/', api.datasource_search, name="api_search_datasource"),
 
     # Meta info:
-    url(r'^api/userinfo?$', 'search.models.api.get_user_info', name="api_user_info"),
-    url(r'^api/preferences?$', 'search.models.api.preferences', name="api_preferences"),
+    url(r'^api/userinfo?$', api.get_user_info, name="api_user_info"),
+    url(r'^api/preferences?$', api.preferences, name="api_preferences"),
 
     # Card
-    url(r'^api/cards/create$', 'search.models.api.card_create', name="api_card_create"),
-    url(r'^api/cards/?$', 'search.models.api.cards', name="api_cards"),
-    url(r'^api/card/(?P<pk>\d+)/add/?$', 'search.models.api.card_add',
+    url(r'^api/cards/create$', api.card_create, name="api_card_create"),
+    url(r'^api/cards/?$', api.cards, name="api_cards"),
+    url(r'^api/card/(?P<pk>\d+)/add/?$', api.card_add,
         name="api_card_add"),
-    url(r'^api/card/(?P<pk>\d+)/reviews/?$', 'search.models.api.card_reviews',
+    url(r'^api/card/(?P<pk>\d+)/reviews/?$', api.card_reviews,
         name="api_card_reviews"),
-    url(r'^api/card/(?P<pk>\d+)', 'search.models.api.card', name="api_card"),
-    url(r'^api/cardtype$', 'search.models.api.cardtype', name="api_cardtype"),
+    url(r'^api/card/(?P<pk>\d+)', api.card, name="api_card"),
+    url(r'^api/cardtype$', api.cardtype, name="api_cardtype"),
     # Bulk manipulation.
-    url(r'^api/cards/set_supplier$', 'search.models.api.cards_set_supplier', name="api_cards_set_supplier"),
+    url(r'^api/cards/set_supplier$', api.cards_set_supplier, name="api_cards_set_supplier"),
 
     # Shelves
-    url(r'^api/shelfs', 'search.models.api.shelfs', name="api_shelfs"),
+    url(r'^api/shelfs', api.shelfs, name="api_shelfs"),
 
     # Authors
-    url(r'^api/authors$', 'search.models.api.authors', name="api_authors"),
+    url(r'^api/authors$', api.authors, name="api_authors"),
 
     # Baskets
-    url(r'^api/baskets/auto_command/open$', 'search.models.api.auto_command_total', name="api_auto_command_total"),
-    url(r'^api/baskets/create', 'search.models.api.baskets_create', name="api_baskets_create"),
-    url(r'^api/baskets/(?P<pk>\d+)/copies', 'search.models.api.basket', name="api_basket_copies"),
-    url(r'^api/baskets/(?P<pk>\d+)/update/?', 'search.models.api.baskets_update', name="api_basket_update"),
-    url(r'^api/v2/baskets/(?P<pk>\d+)/add/?', 'search.models.api.baskets_add_card', name="api_basket_add_card"),
-    url(r'^api/baskets/(?P<pk>\d+)/return/?', 'search.models.api.baskets_return', name="api_basket_return"),
-    url(r'^api/baskets/(?P<pk>\d+)/add_to_shelf/?', 'search.models.api.baskets_add_to_shelf', name="api_basket_add_to_shelf"),
-    url(r'^api/baskets/(?P<pk>\d+)/archive/?', 'search.models.api.baskets_archive', name="api_basket_archive"),
-    url(r'^api/baskets/(?P<pk>\d+)/delete/?', 'search.models.api.baskets_delete', name="api_basket_delete"),
-    url(r'^api/baskets/?$', 'search.models.api.baskets', name="api_baskets"),
+    url(r'^api/baskets/auto_command/open$', api.auto_command_total, name="api_auto_command_total"),
+    url(r'^api/baskets/create', api.baskets_create, name="api_baskets_create"),
+    url(r'^api/baskets/(?P<pk>\d+)/copies', api.basket, name="api_basket_copies"),
+    url(r'^api/baskets/(?P<pk>\d+)/update/?', api.baskets_update, name="api_basket_update"),
+    url(r'^api/v2/baskets/(?P<pk>\d+)/add/?', api.baskets_add_card, name="api_basket_add_card"),
+    url(r'^api/baskets/(?P<pk>\d+)/return/?', api.baskets_return, name="api_basket_return"),
+    url(r'^api/baskets/(?P<pk>\d+)/add_to_shelf/?', api.baskets_add_to_shelf, name="api_basket_add_to_shelf"),
+    url(r'^api/baskets/(?P<pk>\d+)/archive/?', api.baskets_archive, name="api_basket_archive"),
+    url(r'^api/baskets/(?P<pk>\d+)/delete/?', api.baskets_delete, name="api_basket_delete"),
+    url(r'^api/baskets/?$', api.baskets, name="api_baskets"),
     # Inventories of baskets
     url(r'^api/baskets/(?P<pk>\d+)/inventories/?$',
-        'search.models.api.baskets_inventory_get_or_create', name="api_baskets_inventories"),
+        api.baskets_inventory_get_or_create, name="api_baskets_inventories"),
     # Add or remove card(s)
-    url(r'^api/baskets/(?P<pk>\d+)/(?P<action>[a-z_]+)/(?P<card_id>\d+)?/?', 'search.models.api.basket', name="api_basket_act"),  # action: add, remove, update
+    url(r'^api/baskets/(?P<pk>\d+)/(?P<action>[a-z_]+)/(?P<card_id>\d+)?/?', api.basket, name="api_basket_act"),  # action: add, remove, update
     # Simple info
-    url(r'^api/baskets/(?P<pk>\d+)/?$', 'search.models.api.basket', name="api_basket"),
+    url(r'^api/baskets/(?P<pk>\d+)/?$', api.basket, name="api_basket"),
 
-    url(r'^api/distributors/(?P<pk>\d+)/?$', 'search.models.api.get_distributor', name="api_get_distributor"),
-    url(r'^api/distributors', 'search.models.api.distributors', name="api_distributors"),
-    url(r'^api/publishers/?$', 'search.models.api.publishers', name="api_publishers"),
-    url(r'^api/deposits/?$', 'search.models.api.deposits', name="api_deposits"),
+    url(r'^api/distributors/(?P<pk>\d+)/?$', api.get_distributor, name="api_get_distributor"),
+    url(r'^api/distributors', api.distributors, name="api_distributors"),
+    url(r'^api/publishers/?$', api.publishers, name="api_publishers"),
+    url(r'^api/deposits/?$', api.deposits, name="api_deposits"),
 
     # Commands
-    url(r'^api/commands/ongoing/?$', 'search.models.api.commands_ongoing', name="api_commands_ongoing"),
-    url(r'^api/commands/ongoing', 'search.models.api.commands_ongoing', name="api_commands_ongoing"),
-    url(r'^api/commands/nb_ongoing/?$', 'search.models.api.nb_commands_ongoing', name="api_commands_nb_ongoing"),
-    url(r'^api/commands/create', 'search.models.api.commands_create', name="api_commands_create"),
-    url(r'^api/commands/(?P<pk>\d+)/update/?$', 'search.models.api.commands_update', name="api_commands_update"),
-    url(r'^api/commands/supplier/(?P<pk>\d+)/copies/?$', 'search.models.api.commands_supplier', name="api_commands_supplier"),
+    url(r'^api/commands/ongoing/?$', api.commands_ongoing, name="api_commands_ongoing"),
+    url(r'^api/commands/ongoing', api.commands_ongoing, name="api_commands_ongoing"),
+    url(r'^api/commands/nb_ongoing/?$', api.nb_commands_ongoing, name="api_commands_nb_ongoing"),
+    url(r'^api/commands/create', api.commands_create, name="api_commands_create"),
+    url(r'^api/commands/(?P<pk>\d+)/update/?$', api.commands_update, name="api_commands_update"),
+    url(r'^api/commands/supplier/(?P<pk>\d+)/copies/?$', api.commands_supplier, name="api_commands_supplier"),
     # A command inventory (receive a parcel)
-    url(r'^api/commands/(?P<pk>\d+)/receive/?$', 'search.models.api.command_receive', name="api_command_receive"),
-    url(r'^api/commands/(?P<pk>\d+)/receive/remove/?$', 'search.models.api.command_receive_remove', name="api_command_receive_remove"),
-    url(r'^api/commands/(?P<pk>\d+)/receive/update/?$', 'search.models.api.command_receive_update', name="api_command_receive_update"),
-    url(r'^api/commands/(?P<pk>\d+)/receive/diff/?$', 'search.models.api.command_receive_diff', name="api_command_receive_diff"),
-    url(r'^api/commands/(?P<pk>\d+)/receive/apply/?$', 'search.models.api.command_receive_apply', name="api_command_receive_apply"),
+    url(r'^api/commands/(?P<pk>\d+)/receive/?$', api.command_receive, name="api_command_receive"),
+    url(r'^api/commands/(?P<pk>\d+)/receive/remove/?$', api.command_receive_remove, name="api_command_receive_remove"),
+    url(r'^api/commands/(?P<pk>\d+)/receive/update/?$', api.command_receive_update, name="api_command_receive_update"),
+    url(r'^api/commands/(?P<pk>\d+)/receive/diff/?$', api.command_receive_diff, name="api_command_receive_diff"),
+    url(r'^api/commands/(?P<pk>\d+)/receive/apply/?$', api.command_receive_apply, name="api_command_receive_apply"),
 
 
     # Sell
-    url(r'^api/sell/(?P<pk>\d+)/undo$', 'search.models.api.sell_undo', name="api_sell_undo"),
-    url(r'^api/sell$', 'search.models.api.sell', name="api_sell"),
+    url(r'^api/sell/(?P<pk>\d+)/undo$', api.sell_undo, name="api_sell_undo"),
+    url(r'^api/sell$', api.sell, name="api_sell"),
 
     # Restocking
-    url(r'^api/restocking/nb_ongoing/?$', 'search.models.api.nb_restocking_ongoing', name="api_restocking_nb_ongoing"),
-    url(r'^api/restocking/validate/?$', 'search.models.api.restocking_validate', name="api_restocking_validate"),
-    url(r'^api/restocking/remove/(?P<pk>\d+)', 'search.models.api.restocking_remove', name="api_restocking_remove"),
+    url(r'^api/restocking/nb_ongoing/?$', api.nb_restocking_ongoing, name="api_restocking_nb_ongoing"),
+    url(r'^api/restocking/validate/?$', api.restocking_validate, name="api_restocking_validate"),
+    url(r'^api/restocking/remove/(?P<pk>\d+)', api.restocking_remove, name="api_restocking_remove"),
 
     # Sells history
-    url(r'^api/history/sells/?$', 'search.models.api.history_sells', name="api_history_sells"),
-    url(r'^api/history/entries/?$', 'search.models.api.history_entries', name="api_history_entries"),
-    url(r'^api/alerts$', 'search.models.api.alerts', name="api_alerts"),
-    url(r'^api/alerts/open$', 'search.models.api.alerts_open', name="api_alerts_open"),
-    url(r'^api/places/?$', 'search.models.api.places', name="api_places"),
+    url(r'^api/history/sells/?$', api.history_sells, name="api_history_sells"),
+    url(r'^api/history/entries/?$', api.history_entries, name="api_history_entries"),
+    url(r'^api/alerts$', api.alerts, name="api_alerts"),
+    url(r'^api/alerts/open$', api.alerts_open, name="api_alerts_open"),
+    url(r'^api/places/?$', api.places, name="api_places"),
 
     # Inventories
-    url(r'^api/inventories/create/?$', 'search.models.api.inventories', name="api_inventories"),
-    url(r'^api/inventories/(?P<pk>\d+)/update/', 'search.models.api.inventories_update', name="api_inventories_update"),
-    url(r'^api/inventories/(?P<pk>\d+)/remove/', 'search.models.api.inventories_remove', name="api_inventories_remove"),
-    url(r'^api/inventories/(?P<pk>\d+)/diff/', 'search.models.api.inventory_diff', name="api_inventories_diff"),
-    url(r'^api/inventories/(?P<pk>\d+)/apply/?', 'search.models.api.inventory_apply', name="api_inventories_apply"),
-    url(r'^api/inventories/(?P<pk>\d+)/copies/?', 'search.models.api.inventories_copies', name="api_inventories_copies"),
-    url(r'^api/inventories/(?P<pk>\d+)/?$', 'search.models.api.inventories', name="api_inventories"),
-    url(r'^api/inventories/?', 'search.models.api.inventories', name="api_inventories"),
+    url(r'^api/inventories/create/?$', api.inventories, name="api_inventories"),
+    url(r'^api/inventories/(?P<pk>\d+)/update/', api.inventories_update, name="api_inventories_update"),
+    url(r'^api/inventories/(?P<pk>\d+)/remove/', api.inventories_remove, name="api_inventories_remove"),
+    url(r'^api/inventories/(?P<pk>\d+)/diff/', api.inventory_diff, name="api_inventories_diff"),
+    url(r'^api/inventories/(?P<pk>\d+)/apply/?', api.inventory_apply, name="api_inventories_apply"),
+    url(r'^api/inventories/(?P<pk>\d+)/copies/?', api.inventories_copies, name="api_inventories_copies"),
+    url(r'^api/inventories/(?P<pk>\d+)/?$', api.inventories, name="api_inventories"),
+    url(r'^api/inventories/?', api.inventories, name="api_inventories"),
 
     # Stats
-    url(r'^api/stats/sells/month', 'search.models.api.stats_sells_month', name='api_stats_sells_month'),
-    url(r'^api/stats/', 'search.models.api.stats', name='api_stats'),
+    url(r'^api/stats/sells/month', api.stats_sells_month, name='api_stats_sells_month'),
+    url(r'^api/stats/', api.stats, name='api_stats'),
 
     # Clients
-    url(r'^api/clients/?', 'search.models.api_users.clients', name='api_clients'),
+    # url(r'^api/clients/?', api.users.clients, name='api_clients'),
 
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -136,22 +139,25 @@ apipatterns = patterns("",
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)  # pylint: disable=bad-continuation
+]  # pylint: disable=bad-continuation
 
 urlpatterns += apipatterns
+# urlpatterns = apipatterns
 
+from django.contrib.auth import views as auth_views
 # Authentication
 urlpatterns += [
-    url("^login/", "django.contrib.auth.views.login",
+    url("^login/", auth_views.LoginView.as_view(),
         {"template_name": "registration/login.jade"},
         name="login"),
-    url("^logout/", "django.contrib.auth.views.logout",
+    url("^logout/", auth_views.LogoutView.as_view(),
         {"template_name": "registration/logout.jade", },
         name="logout"),
 ]
 
 # the following does include a login/ too, but the first one will match.
 urlpatterns += [url('^', include('django.contrib.auth.urls'))]
+
 
 # Serve media (images).
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- search/urls.py
+++ search/urls.py
@@ -14,9 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Abelujo.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf.urls import include
-from django.conf.urls import patterns
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+from django.views import i18n
+from django.conf.urls.i18n import i18n_patterns
 from django.views.generic import TemplateView
 # from django.views.generic.base import RedirectView
 from search import views
@@ -24,30 +25,38 @@ from search.admin import admin_site
 
 js_info_dict = {'packages': ('search', '',), }
 
-# i18n_patterns is taken care of in the root url conf.
-urlpatterns = patterns('',
+from django.views.i18n import JavaScriptCatalog
+
+app_name = 'search'  # test
+
+urlpatterns = [
+    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+]
+
+# i18n_patterns is taken care of in the root url conf and in abelujo.urls.
+urlpatterns += [
     #pylint: disable=bad-continuation
     # Access to the translations in javascript code:
-    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict,),
+    # url(r'^jsi18n/$', i18n.javascript_catalog, js_info_dict,),
     # Django Rest Framework browsable api
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     # url(r'^$', RedirectView.as_view(url='search/')),
-    url(r'^$', 'search.views.dashboard', name="dashboard"),
+    url(r'^$', views.dashboard, name="dashboard"),
     url(r'^database/?', admin_site.urls),
 
-    url(r'^preferences/?$', 'search.views.preferences', name="preferences"),
+    url(r'^preferences/?$', views.preferences, name="preferences"),
 
-    url(r'^search/?$', 'search.views.search', name="card_search"),
+    url(r'^search/?$', views.search, name="card_search"),
 
-    url(r'^stats/', 'search.views.dashboard', name="stats"),
+    url(r'^stats/', views.dashboard, name="stats"),
 
     # view exported files:
-    url(r'^stock/exports/download/(?P<filename>.+)', 'search.views.collection_view_file_export', name="collection_view_file_export"),
-    url(r'^stock/exports/?', 'search.views.collection_exports', name="collection_exports"),
-    url(r'^stock/export', 'search.views.collection_export', name="collection_export"),
+    url(r'^stock/exports/download/(?P<filename>.+)', views.collection_view_file_export, name="collection_view_file_export"),
+    url(r'^stock/exports/?', views.collection_exports, name="collection_exports"),
+    url(r'^stock/export', views.collection_export, name="collection_export"),
 
-    url(r'^stock/card/(?P<pk>\d+)/?$', 'search.views.card_show',
+    url(r'^stock/card/(?P<pk>\d+)/?$', views.card_show,
         name="card_show"),
     # works to edit a card with /edit/\d+. JS will fetch the existing info.
     # url(r'^stock/card/edit/', login_required(TemplateView.as_view(template_name="search/card_edit.jade")),
@@ -55,46 +64,46 @@ urlpatterns = patterns('',
         login_required(TemplateView.as_view(template_name="search/card_add.jade")),
         name="card_create"),
     # Add exemplaries to Places, from the Card view.
-    url(r'^stock/card/add/(?P<pk>\d+)', 'search.views.card_places_add',
+    url(r'^stock/card/add/(?P<pk>\d+)', views.card_places_add,
         name="card_places_add"),
     url(r'^stock/card/edit/(?P<pk>\d+)?',
         login_required(TemplateView.as_view(template_name="search/card_edit.jade")),
         name="card_edit"),
-    url(r'^stock/card/(?P<pk>\d+)/move', 'search.views.card_move',
+    url(r'^stock/card/(?P<pk>\d+)/move', views.card_move,
         name="card_move"),
-    url(r'^stock/card/(?P<pk>\d+)/history', 'search.views.card_history',
+    url(r'^stock/card/(?P<pk>\d+)/history', views.card_history,
         name="card_history"),
-    url(r'^stock/card/(?P<pk>\d+)/buy', 'search.views.card_buy',
+    url(r'^stock/card/(?P<pk>\d+)/buy', views.card_buy,
         name="card_buy"),
-    url(r'^stock/set_supplier/', 'search.views.cards_set_supplier',
+    url(r'^stock/set_supplier/', views.cards_set_supplier,
         name="cards_set_supplier"),
 
-    url(r'^sell/$', 'search.views.sell',
+    url(r'^sell/$', views.sell,
         name="card_sell"),
-    url(r'^sell/(?P<pk>\d+)', 'search.views.sell_details',
+    url(r'^sell/(?P<pk>\d+)', views.sell_details,
         name="sell_details"),
 
-    url(r'^restocking/$', 'search.views.restocking',
+    url(r'^restocking/$', views.restocking,
         name="card_restocking"),
 
-    url(r'^collection/', 'search.views.collection',
+    url(r'^collection/', views.collection,
         name="card_collection"),
 
     url(r'^deposits/$', login_required(views.DepositsListView.as_view()),
         name="deposits"),
-    url(r'^deposits/addcard', "search.views.deposits_add_card",
+    url(r'^deposits/addcard', views.deposits_add_card,
         name="deposits_add_card"),
-    url(r'^deposits/new', 'search.views.deposits_new',
+    url(r'^deposits/new', views.deposits_new,
         name="deposits_new"),
-    url(r'^deposits/create', 'search.views.deposits_create',
+    url(r'^deposits/create', views.deposits_create,
         name="deposits_create"),
-    url(r'^deposits/(?P<pk>\d+)/checkout', 'search.views.deposits_checkout',
+    url(r'^deposits/(?P<pk>\d+)/checkout', views.deposits_checkout,
         name="deposit_checkout"),
-    url(r'^deposits/(\d+)/?$', 'search.views.deposits_view',
+    url(r'^deposits/(\d+)/?$', views.deposits_view,
         name="deposits_view"),
-    # url(r'^deposits/(?P<pk>\d+)/delete', 'search.views.deposit_delete',
+    # url(r'^deposits/(?P<pk>\d+)/delete', views.deposit_delete,
     # name="deposit_delete"),
-    url(r'^deposits/(?P<pk>\d+)/add', 'search.views.deposit_add_copies',
+    url(r'^deposits/(?P<pk>\d+)/add', views.deposit_add_copies,
         name="deposit_add_copies"),
 
     # Commands:
@@ -104,60 +113,60 @@ urlpatterns = patterns('',
     url(r'^commands/(?P<pk>\d+)/receive/terminate/?$', views.command_receive_terminate, name="commands_receive_terminate"),
     url(r'^commands/(?P<pk>\d+)/receive?$', views.command_receive, name="commands_receive"),
     url(r'^commands/(?P<pk>\d+)/?$', login_required(views.CommandDetailView.as_view()), name="commands_view"),
-    url(r'^commands/supplier/(?P<pk>\d+)/?$', 'search.views.command_supplier',
+    url(r'^commands/supplier/(?P<pk>\d+)/?$', views.command_supplier,
         name="command_supplier"),
-    url(r'^commands/?$', 'search.views.basket_auto_command',
+    url(r'^commands/?$', views.basket_auto_command,
         name="basket_auto_command"),
 
     url(r'^command/(?P<pk>\d+)/?', login_required(views.command_card)),
 
     # Baskets:
-    url(r'^baskets/(?P<pk>\d+)/export/$', 'search.views.basket_export', name="basket_export"),
+    url(r'^baskets/(?P<pk>\d+)/export/$', views.basket_export, name="basket_export"),
     url(r'^baskets/(?P<pk>\d+)/receive/$',
         login_required(TemplateView.as_view(template_name="search/inventory_view.jade"))),
-    url(r'^baskets/$', 'search.views.baskets',
+    url(r'^baskets/$', views.baskets,
         name="baskets"),
     # Endpoints for vue, to replace the previous ones.
     # Simple presentation:
-    url(r'^lists/?$', 'search.views.basket_list', name="basket_list"),
+    url(r'^lists/?$', views.basket_list, name="basket_list"),
     # Vue app
     url(r'^lists/(?P<pk>\d+)/?$', login_required(views.basket_view), name="basket_view"),
 
     # Sells history
-    url(r'^history/sells/month/(?P<date>.*)/?', 'search.views.history_sells_month', name="history_sells_month"),
-    url(r'^history/sells/day/(?P<date>.*)/?', 'search.views.history_sells_day', name="history_sells_day"),
-    url(r'^history/sells/export/?', 'search.views.history_sells_exports',
+    url(r'^history/sells/month/(?P<date>.*)/?', views.history_sells_month, name="history_sells_month"),
+    url(r'^history/sells/day/(?P<date>.*)/?', views.history_sells_day, name="history_sells_day"),
+    url(r'^history/sells/export/?', views.history_sells_exports,
         name="history_sells_exports"),
 
     # Entries history
-    url(r'^history/entries/month/(?P<date>.*)/?', 'search.views.history_entries_month', name="history_entries_month"),
-    url(r'^history/entries/day/(?P<date>.*)/?', 'search.views.history_entries_day', name="history_entries_day"),
+    url(r'^history/entries/month/(?P<date>.*)/?', views.history_entries_month, name="history_entries_month"),
+    url(r'^history/entries/day/(?P<date>.*)/?', views.history_entries_day, name="history_entries_day"),
 
-    url(r'^history/?', 'search.views.history_sells', name="history_sells"),
+    url(r'^history/?', views.history_sells, name="history_sells"),
     # url(r'^history/', login_required(TemplateView.as_view(template_name="search/history.jade")),
     # name="search_history"),
 
     # Suppliers: sells of suppliers by month.
-    url(r'^suppliers/sells/month/(?P<date>.*)/?', 'search.views.suppliers_sells_month', name="suppliers_sells_month"),
-    url(r'^publishers/(?P<pk>\d+)/sells/month/(?P<date>.*)/?', 'search.views.publisher_sells_month_list', name="publisher_sells_month_list"),
-    url(r'^distributors/(?P<pk>\d+)/sells/month/(?P<date>.*)/?', 'search.views.distributors_sells_month_list', name="distributors_sells_month_list"),
-    url(r'^suppliers/?', 'search.views.suppliers_sells', name="suppliers_sells"),
+    url(r'^suppliers/sells/month/(?P<date>.*)/?', views.suppliers_sells_month, name="suppliers_sells_month"),
+    url(r'^publishers/(?P<pk>\d+)/sells/month/(?P<date>.*)/?', views.publisher_sells_month_list, name="publisher_sells_month_list"),
+    url(r'^distributors/(?P<pk>\d+)/sells/month/(?P<date>.*)/?', views.distributors_sells_month_list, name="distributors_sells_month_list"),
+    url(r'^suppliers/?', views.suppliers_sells, name="suppliers_sells"),
 
     url(r'^alerts/', login_required(TemplateView.as_view(template_name="search/alerts.jade")),
         name="search_alerts"),
 
-    url(r'^inventories/$', 'search.views.inventories',
+    url(r'^inventories/$', views.inventories,
         name="inventories"),
     url(r'^inventories/new$', login_required(TemplateView.as_view(template_name="search/inventory_view.jade")),
         name="inventory_new"),
-    url(r'^inventories/(?P<pk>\d+)/?$', 'search.views.inventory',
+    url(r'^inventories/(?P<pk>\d+)/?$', views.inventory,
         name="inventory_view"),
-    url(r'^inventories/(?P<pk>\d+)/archive', 'search.views.inventory_archive',
+    url(r'^inventories/(?P<pk>\d+)/archive', views.inventory_archive,
         name="inventory_archive"),
-    url(r'^inventories/(?P<pk>\d+)/delete', 'search.views.inventory_delete',
+    url(r'^inventories/(?P<pk>\d+)/delete', views.inventory_delete,
         name="inventory_delete"),
-    url(r'^inventories/(?P<pk>\d+)/terminate', 'search.views.inventory_terminate',
+    url(r'^inventories/(?P<pk>\d+)/terminate', views.inventory_terminate,
         name="inventory_terminate"),
-    url(r'^inventories/(?P<pk>\d+)/export', 'search.views.inventory_export',
+    url(r'^inventories/(?P<pk>\d+)/export', views.inventory_export,
         name="inventory_export"),
-)
+]
