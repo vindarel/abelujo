1b25041ef75b0ea103e523383e68aad2e46156ae
vindarel <vindarel@mailz.org>
2021-03-16 12:53:13 +0100

    fix migration: default payment_2 is "0", not "(7, ESPECES)"
    [ci skip]
    
    otherwise, do:
    
    res = Sell.objects.filter(payment_2__startswith="(")
    res.update(payment_2="0")

--- search/migrations/0020_auto_20210209_1524.py
+++ search/migrations/0020_auto_20210209_1524.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='sell',
             name='payment_2',
-            field=models.CharField(default=(7, b'ESP\xc3\x88CES'), choices=[(7, b'ESP\xc3\x88CES'), (9, b'CB'), (8, b'CH\xc3\x88QUE'), (10, b'transfert'), (100, b"bon d'achat"), (11, b'autre')], max_length=200, blank=True, null=True, verbose_name='payment'),
+            field=models.CharField(default="0", choices=[(7, b'ESP\xc3\x88CES'), (9, b'CB'), (8, b'CH\xc3\x88QUE'), (10, b'transfert'), (100, b"bon d'achat"), (11, b'autre')], max_length=200, blank=True, null=True, verbose_name='payment'),
         ),
         migrations.AddField(
             model_name='sell',
