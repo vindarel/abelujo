573d85138f0610009e3262322ae2ff1cef3cd430
vindarel <vindarel@mailz.org>
2021-04-28 11:22:38 +0200

    admin, Client list: show email
    [ci skip]

--- search/admin.py
+++ search/admin.py
@@ -69,7 +69,7 @@ class ClientAdmin(admin.ModelAdmin):
         model = Client
 
     search_fields = ["name", "firstname", "mobilephone", "address1", "city", "country", "zip_code"]
-    list_display = ["name", "firstname", "mobilephone"]
+    list_display = ["name", "firstname", "mobilephone", "email"]
     ordering = ["name", "firstname"]
 
 
