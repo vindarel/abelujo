192e22386823117236781ed61c9c4c52380623b2
vindarel <vindarel@mailz.org>
2021-07-21 13:55:26 +0200

    management command: delete test places
    [ci skip]

---
+++ search/management/commands/delete_test_places.py
@@ -0,0 +1,33 @@
+# Abelujo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Abelujo.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Delete test places that are created by the buggy test runner...
+"""
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+
+from search.models import Preferences, Place
+
+# py2/3
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        Place.delete_test_places()
+        self.stdout.write("OK")
