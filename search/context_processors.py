bfcc8f6e72de0d1fdb8055e39f6f359e6c1090ce
vindarel <vindarel@mailz.org>
2021-09-09 13:44:32 +0200

    Add footer, show Abelujo's git tag version
    [ci skip]

--- search/context_processors.py
+++ search/context_processors.py
@@ -16,11 +16,32 @@
 # along with Abelujo.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
+import subprocess
+
 from django.conf import settings
+
+ABELUJO_VERSION = None
+
+def get_abelujo_version():
+    """
+    Read Abelujo's version from its git tag on startup.
+
+    Return: a string, like '0.69-283-g35f72d4b\n'.
+    """
+    version = None
+    try:
+        version = subprocess.check_output("git describe --always --tags", shell=True)
+        global ABELUJO_VERSION
+        ABELUJO_VERSION = version
+    except Exception as e:
+        print("Could not get abelujo git tag version: \n {}".format(e))
+
+get_abelujo_version()
 
 
 def global_settings(request):
     # return any necessary values
     return {
         'FEATURE_EXCLUDE_FOR_WEBSITE': settings.FEATURE_EXCLUDE_FOR_WEBSITE,
+        'ABELUJO_VERSION': ABELUJO_VERSION,
     }
